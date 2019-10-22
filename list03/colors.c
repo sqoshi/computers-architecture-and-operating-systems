e3ad3c27116e84711d1477bbb792eba138a813f7
Piotr Popis <piotrpopis98@icloud.com>
2019-10-22 23:02:08 +0200

    added list03

---
+++ list03/colors.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (int argc, char const *argv[]) {
+ for(int i=0;i<=256;i++) 
+  		printf("\x1b[38;5;" "%d" "m Hello World!" "\x1b[0m" 
+  		"\n",i);
+/*	for(int j=0;j<8;j++) printf("\x1b[4%dm Hello World! \x1b[0m 
+		\n",j);
+	for(int k=0;k<=8;k++) printf("\x1b[%dm Hello World!\x1b[0m 
+		\n",k);
+  */ return 0;
+}
